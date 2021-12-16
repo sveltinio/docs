7bad559ac02fd2751d7d07efc95806aa6b83ab36
indaco <github@mircoveltri.me>
2021-12-16 23:05:52 +0100

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -1,9 +1,42 @@
 const menu = [
 	{
-		identifier: 'home',
-		name: 'Home',
-		url: '/',
+		identifier: 'about',
+		name: 'About Sveltin',
+		url: '/about',
 		weight: 1
+	},
+
+	{
+		identifier: 'getting-started',
+		name: 'Getting Started',
+		url: '/getting-started',
+		weight: 2
+	},
+	{
+		identifier: 'tutorials',
+		name: 'Tutorials',
+		url: '/tutorials',
+		weight: 3
+	},
+	{
+		identifier: 'cli',
+		name: 'CLI',
+		url: '/cli',
+		weight: 4,
+		children: [
+			{
+				identifier: 'new-project',
+				name: 'new project',
+				url: '/cli/new-project',
+				weight: 1
+			},
+			{
+				identifier: 'new-page',
+				name: 'new page',
+				url: '/cli/new-page',
+				weight: 2
+			}
+		]
 	}
 ];
 
