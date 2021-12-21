82683b2fa2126479b650d329d9bd4b9af172a37f
Mirco Veltri <indaco@users.noreply.github.com>
2021-12-21 22:09:54 +0100

    Create external_links.js

---
+++ config/external_links.js
@@ -0,0 +1,10 @@
+const external_links = [
+	{
+		identifier: 'github',
+		name: 'GitHub Repo',
+		url: 'http://github.com/sveltinio/sveltin',
+		weight: 1
+	}
+];
+
+export { external_links as default };
