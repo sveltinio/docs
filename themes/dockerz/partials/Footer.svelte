927d6572a04476f9cf5cee003b17fb337c685d06
Mirco Veltri <indaco@users.noreply.github.com>
2021-12-21 22:14:04 +0100

    Update Footer.svelte

--- themes/dockerz/partials/Footer.svelte
+++ themes/dockerz/partials/Footer.svelte
@@ -1,183 +1,15 @@
 <script lang="ts">
+	import type { MenuItem, ExternalLinkItem } from '$lib/schema';
+	import FooterLinks from '$themes/dockerz/components/_FooterLinks.svelte';
 	import ModernImage from '$components/_ModernImage.svelte';
-	import { website } from '$config/website';
-
+	
 	export let cliVersion: string;
+	export let websiteData;
+	export let menuData: Array<MenuItem>;
+	export let externalLinksData: Array<ExternalLinkItem>;
 </script>
 
-<div
-	class="border-t border-antiflash bg-skin-base dark:border-skin-muted-dark dark:bg-skin-base-dark"
->
-	<div class="mx-auto max-w-7xl">
-		<div class="grid grid-cols-2 mx-auto max-w-none">
-			<div
-				class="
-          px-4
-          py-8
-          bg-skin-base
-          dark:bg-skin-base-dark dark:border-skin-base-dark
-          sm:px-6
-          lg:px-8
-        "
-			>
-				<div class="grid grid-cols-1 lg:grid-cols-3">
-					<ul role="list" class="mt-4 space-y-4 lg:mt-0">
-						<li>
-							<a
-								href="./index.html"
-								class="
-                  text-xs
-                  font-light
-                  text-skin-muted
-                  dark:text-skin-muted-dark
-                  hover:text-skin-muted-hover
-                  dark:hover:text-skin-muted-dark-hover
-                  sm:text-xl
-                  lg:text-base
-                "
-							>
-								Home
-							</a>
-						</li>
-						<li>
-							<a
-								href="./features.html"
-								class="
-                  text-xs
-                  font-light
-                  text-skin-muted
-                  dark:text-skin-muted-dark
-                  hover:text-skin-muted-hover
-                  dark:hover:text-skin-muted-dark-hover
-                  sm:text-xl
-                  lg:text-base
-                "
-							>
-								Features
-							</a>
-						</li>
-						<li>
-							<a
-								href="./about.html"
-								class="
-                  text-xs
-                  font-light
-                  text-skin-muted
-                  dark:text-skin-muted-dark
-                  hover:text-skin-muted-hover
-                  dark:hover:text-skin-muted-dark-hover
-                  sm:text-xl
-                  lg:text-base
-                "
-							>
-								About
-							</a>
-						</li>
-					</ul>
-					<ul role="list" class="mt-4 space-y-4 lg:mt-0">
-						<li>
-							<a
-								href="./index.html"
-								class="
-                  text-xs
-                  font-light
-                  text-skin-muted
-                  dark:text-skin-muted-dark
-                  hover:text-skin-muted-hover
-                  dark:hover:text-skin-muted-dark-hover
-                  sm:text-xl
-                  lg:text-base
-                "
-							>
-								Home
-							</a>
-						</li>
-						<li>
-							<a
-								href="./features.html"
-								class="
-                  text-xs
-                  font-light
-                  text-skin-muted
-                  dark:text-skin-muted-dark
-                  hover:text-skin-muted-hover
-                  dark:hover:text-skin-muted-dark-hover
-                  sm:text-xl
-                  lg:text-base
-                "
-							>
-								Features
-							</a>
-						</li>
-						<li>
-							<a
-								href="./about.html"
-								class="
-                  text-xs
-                  font-light
-                  text-skin-muted
-                  dark:text-skin-muted-dark
-                  hover:text-skin-muted-hover
-                  dark:hover:text-skin-muted-dark-hover
-                  sm:text-xl
-                  lg:text-base
-                "
-							>
-								About
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div
-				class="
-          px-4
-          py-8
-          md:mt-0
-          bg-skin-base
-          dark:bg-skin-base-dark dark:border-skin-base-dark
-          sm:px-6
-          lg:px-8
-        "
-				id="contact"
-			>
-				<div class="mt-8 md:mt-0 md:order-1 lg:pr-12">
-					<p
-						class="
-              text-xs
-              font-light
-              text-skin-muted
-              dark:text-skin-muted-dark
-              sm:text-xl
-              lg:text-base
-            "
-					>
-						Have questions? Email us at email@example.com
-					</p>
-					<p
-						class="
-              text-xs
-              font-light
-              text-skin-muted
-              dark:text-skin-muted-dark
-              sm:text-xl
-              lg:text-base
-            "
-					>
-						Dockerz ©
-
-						<span x-text="new Date().getFullYear()" />
-						<a
-							href="https://wickedlabs.dev/"
-							class="text-skin-link hover:text-skin-link-hover">Wicked Labs</a
-						>
-						All rights reserved.
-					</p>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+<FooterLinks {menuData} {externalLinksData} />
 
 <footer
 	class="border-t border-antiflash bg-skin-base dark:bg-skin-base-dark dark:border-skin-muted-dark"
@@ -188,18 +20,11 @@
 		<div class="flex justify-center text-center lg:text-left lg:justify-between">
 			<div class="space-y-8 text-skin-muted dark:text-skin-muted-sard">
 				<div
-					class="
-            inline-flex
-            justify-between
-            items-center
-            w-full
-            font-bold
-            tracking-tighter
-          "
+					class="inline-flex justify-between items-center w-full font-bold tracking-tighter"
 				>
 					<ModernImage
-						pathToFile={website.logo}
-						altText="{website.title} logo"
+						pathToFile={websiteData.logo}
+						altText="{websiteData.title} logo"
 						webp
 						avif={false}
 						class="w-28 h-auto lazyload"
