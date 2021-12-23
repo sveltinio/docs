61b0e2d2a9773fdae4e2c8248d0c2779a29b219c
Mirco Veltri <indaco@users.noreply.github.com>
2021-12-23 10:42:13 +0100

    Update external_links.js

--- config/external_links.js
+++ config/external_links.js
@@ -1,9 +1,15 @@
 const external_links = [
 	{
+		identifier: 'landing',
+		name: 'Home page',
+		url: 'http://sveltin.io',
+		weight: 1
+	},
+	{
 		identifier: 'github',
 		name: 'GitHub Repo',
 		url: 'http://github.com/sveltinio/sveltin',
-		weight: 1
+		weight: 2
 	}
 ];
 
