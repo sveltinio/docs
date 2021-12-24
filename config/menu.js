af748cd70b9bfef552ef37b0ba7bb05340bfd9d4
indaco <github@mircoveltri.me>
2021-12-24 17:14:39 +0100

    CLI commands content added

--- config/menu.js
+++ config/menu.js
@@ -25,16 +25,108 @@ const menu = [
 		weight: 4,
 		children: [
 			{
-				identifier: 'new-project',
-				name: 'new project',
-				url: '/cli/new-project',
+				identifier: 'sveltin',
+				name: 'Sveltin',
+				url: '/cli/sveltin',
 				weight: 1
 			},
+
+			{
+				identifier: 'new',
+				name: 'New',
+				url: '/cli/new',
+				weight: 2
+			},
+
+			{
+				identifier: 'new-resource',
+				name: 'New-Resource',
+				url: '/cli/new-resource',
+				weight: 3
+			},
+
+			{
+				identifier: 'new-content',
+				name: 'New-Content',
+				url: '/cli/new-content',
+				weight: 4
+			},
+
+			{
+				identifier: 'new-metadata',
+				name: 'New-Metadata',
+				url: '/cli/new-metadata',
+				weight: 5
+			},
+
 			{
 				identifier: 'new-page',
-				name: 'new page',
+				name: 'New-Page',
 				url: '/cli/new-page',
-				weight: 2
+				weight: 6
+			},
+
+			{
+				identifier: 'new-theme',
+				name: 'New-Theme',
+				url: '/cli/new-theme',
+				weight: 7
+			},
+
+			{
+				identifier: 'generate',
+				name: 'Generate',
+				url: '/cli/generate',
+				weight: 8
+			},
+
+			{
+				identifier: 'generate-menu',
+				name: 'Generate-Menu',
+				url: '/cli/generate-menu',
+				weight: 9
+			},
+
+			{
+				identifier: 'generate-rss',
+				name: 'Generate-Rss',
+				url: '/cli/generate-rss',
+				weight: 10
+			},
+
+			{
+				identifier: 'generate-sitemap',
+				name: 'Generate-Sitemap',
+				url: '/cli/generate-sitemap',
+				weight: 11
+			},
+
+			{
+				identifier: 'server',
+				name: 'Server',
+				url: '/cli/server',
+				weight: 12
+			},
+
+			{
+				identifier: 'prepare',
+				name: 'Prepare',
+				url: '/cli/prepare',
+				weight: 13
+			},
+
+			{
+				identifier: 'build',
+				name: 'Build',
+				url: '/cli/build',
+				weight: 14
+			},
+
+			{
+				identifier: 'preview',
+				name: 'Preview',
+				url: '/cli/preview',
+				weight: 15
 			}
 		]
 	}
