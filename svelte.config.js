b14275afc1ee2a73dbeab86e98727b699604760f
indaco <github@mircoveltri.me>
2022-01-25 11:49:50 +0100

    chore: precompress option for adapter-static set to true

--- svelte.config.js
+++ svelte.config.js
@@ -22,7 +22,8 @@ const config = {
 			// default options are shown
 			pages: 'build',
 			assets: 'build',
-			fallback: null
+			fallback: null,
+			precompress: true
 		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
