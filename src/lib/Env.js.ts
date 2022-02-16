f23e51ee47b5ecec896d4cb680aa8075e6a7973c
indaco <github@mircoveltri.me>
2022-02-16 23:30:04 +0100

    chore: sveltekit process env variable added

--- src/lib/Env.js.ts
+++ src/lib/Env.js.ts
@@ -1,3 +1,4 @@
 export const siteConfig = {
-	baseURL: import.meta.env.VITE_PUBLIC_BASE_PATH
+	baseURL: import.meta.env.VITE_PUBLIC_BASE_PATH,
+	skVersion: process.env.npm_package_devDependencies__sveltejs_kit
 };
