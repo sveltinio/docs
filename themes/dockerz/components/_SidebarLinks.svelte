d10478bf1010df6ffaaca9e2230adbb32d3c6c84
indaco <github@mircoveltri.me>
2022-02-16 17:39:10 +0100

    docs: build info added to the sidebar

--- themes/dockerz/components/_SidebarLinks.svelte
+++ themes/dockerz/components/_SidebarLinks.svelte
@@ -53,6 +53,23 @@
 		{/each}
 	</div>
 	<SidebarExternals externalLinksData={externalLinks} />
+	<div class="pl-6 mt-12">
+		<p class="text-skin-heading dark:text-skin-heading-dark text-xs font-semibold uppercase">
+			Build Info
+		</p>
+	</div>
+	<div class="mt-3">
+		<p
+			class="p-2 pl-6 text-sm font-light text-skin-base dark:hover:text-skin-base-dark-hover dark:text-skin-base-dark dark:hover:bg-skin-dark group"
+		>
+			Sveltin v0.3.0
+		</p>
+		<p
+			class="p-2 pl-6 text-sm font-light text-skin-base dark:hover:text-skin-base-dark-hover dark:text-skin-base-dark dark:hover:bg-skin-dark group"
+		>
+			SvelteKit v1.0.0-next.264
+		</p>
+	</div>
 </div>
 
 <style>
