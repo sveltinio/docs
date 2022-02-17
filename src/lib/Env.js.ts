6ee23a49b83ee496290ab3c613d3b4f01b436354
indaco <github@mircoveltri.me>
2022-02-17 11:39:33 +0100

    fix: env.js.ts

--- src/lib/Env.js.ts
+++ src/lib/Env.js.ts
@@ -1,4 +1,3 @@
 export const siteConfig = {
-	baseURL: import.meta.env.VITE_PUBLIC_BASE_PATH,
-	skVersion: process.env.npm_package_devDependencies__sveltejs_kit
+	baseURL: import.meta.env.VITE_PUBLIC_BASE_PATH
 };
