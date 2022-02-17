1dde7789e1925760559885aeff166503bd2b99f3
indaco <github@mircoveltri.me>
2022-02-17 11:39:59 +0100

    chore: update sk version

--- themes/dockerz/components/_BuildInfo.svelte
+++ themes/dockerz/components/_BuildInfo.svelte
@@ -17,6 +17,6 @@
 	<p
 		class="p-2 pl-6 text-sm font-light text-skin-base dark:hover:text-skin-base-dark-hover dark:text-skin-base-dark dark:hover:bg-skin-dark group"
 	>
-		SvelteKit v1.0.0-next.275
+		SvelteKit v1.0.0-next.276
 	</p>
 </div>
