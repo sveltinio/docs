2fbbe1a711c1260fe29d7bb0d0094b653b9a48ce
indaco <github@mircoveltri.me>
2022-03-07 15:51:54 +0100

    docs(menu): commands order updated

--- config/menu.js.ts
+++ config/menu.js.ts
@@ -50,88 +50,94 @@ const menu: Array<IMenuItem> = [
 				weight: 2
 			},
 			{
-				identifier: 'new-resource',
-				name: 'new resource',
-				url: '/cli/new-resource',
+				identifier: 'install',
+				name: 'install',
+				url: '/cli/install',
 				weight: 3
-			},
-			{
-				identifier: 'new-content',
-				name: 'new content',
-				url: '/cli/new-content',
-				weight: 4
-			},
-			{
-				identifier: 'new-metadata',
-				name: 'new metadata',
-				url: '/cli/new-metadata',
-				weight: 5
-			},
-			{
-				identifier: 'new-page',
-				name: 'new page',
-				url: '/cli/new-page',
-				weight: 6
-			},
-			{
-				identifier: 'generate',
-				name: 'generate',
-				url: '/cli/generate',
-				weight: 7
-			},
-			{
-				identifier: 'generate-menu',
-				name: 'generate menu',
-				url: '/cli/generate-menu',
-				weight: 8
-			},
-			{
-				identifier: 'generate-rss',
-				name: 'generate rss',
-				url: '/cli/generate-rss',
-				weight: 9
-			},
-			{
-				identifier: 'generate-sitemap',
-				name: 'generate sitemap',
-				url: '/cli/generate-sitemap',
-				weight: 10
-			},
-			{
-				identifier: 'server',
-				name: 'server',
-				url: '/cli/server',
-				weight: 11
 			},
 			{
 				identifier: 'prepare',
 				name: 'prepare',
 				url: '/cli/prepare',
+				weight: 4
+			},
+			{
+				identifier: 'new-resource',
+				name: 'new resource',
+				url: '/cli/new-resource',
+				weight: 5
+			},
+			{
+				identifier: 'new-content',
+				name: 'new content',
+				url: '/cli/new-content',
+				weight: 6
+			},
+			{
+				identifier: 'new-metadata',
+				name: 'new metadata',
+				url: '/cli/new-metadata',
+				weight: 7
+			},
+			{
+				identifier: 'new-page',
+				name: 'new page',
+				url: '/cli/new-page',
+				weight: 8
+			},
+			{
+				identifier: 'generate',
+				name: 'generate',
+				url: '/cli/generate',
+				weight: 9
+			},
+			{
+				identifier: 'generate-menu',
+				name: 'generate menu',
+				url: '/cli/generate-menu',
+				weight: 10
+			},
+			{
+				identifier: 'generate-rss',
+				name: 'generate rss',
+				url: '/cli/generate-rss',
+				weight: 11
+			},
+			{
+				identifier: 'generate-sitemap',
+				name: 'generate sitemap',
+				url: '/cli/generate-sitemap',
 				weight: 12
+			},
+			{
+				identifier: 'server',
+				name: 'server',
+				url: '/cli/server',
+				weight: 13
 			},
 			{
 				identifier: 'update',
 				name: 'update',
 				url: '/cli/update',
-				weight: 13
+				weight: 14
 			},
 			{
 				identifier: 'build',
 				name: 'build',
 				url: '/cli/build',
-				weight: 14
+				weight: 15
 			},
 			{
 				identifier: 'preview',
 				name: 'preview',
 				url: '/cli/preview',
-				weight: 15
+				weight: 16
 			},
 			{
 				identifier: 'deploy',
 				name: 'deploy',
 				url: '/cli/deploy',
-				weight: 16
+				weight: 17
 			}
 		]
 	}
