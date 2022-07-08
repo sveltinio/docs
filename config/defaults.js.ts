b59e099c2d665aaba279a94b2564a1b46b7ee7be
Mirco Veltri <github@mircoveltri.me>
2022-07-08 15:41:27 +0200

    chore: upgrade to sveltin v0.8.3

--- config/defaults.js.ts
+++ config/defaults.js.ts
@@ -1,4 +1,4 @@
-const sveltinVersion = '0.8.2';
+const sveltinVersion = '0.8.3';
 const sveltekitVersion = process.env.VITE_SVELTEKIT_VERSION;
 const buildTime = process.env.VITE_BUILD_TIME;
 export { sveltinVersion, sveltekitVersion, buildTime };
