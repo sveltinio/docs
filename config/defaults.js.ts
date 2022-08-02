c4e38003f1b1c48a77b35d23c6383fbab6ba6861
Mirco Veltri <github@mircoveltri.me>
2022-08-02 16:17:01 +0200

    chore: bump sveltin cli version to 0.8.11

--- config/defaults.js.ts
+++ config/defaults.js.ts
@@ -1,4 +1,4 @@
-const sveltinVersion = '0.8.10';
+const sveltinVersion = '0.8.11';
 const sveltekitVersion = process.env.VITE_SVELTEKIT_VERSION;
 const buildTime = process.env.VITE_BUILD_TIME;
 export { sveltinVersion, sveltekitVersion, buildTime };
