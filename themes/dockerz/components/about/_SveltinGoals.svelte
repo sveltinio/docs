b4ead121eb9e8741a9ce298a74d8a3741abe21b2
Mirco Veltri <github@mircoveltri.me>
2022-09-06 15:38:45 +0200

    chore: sveltin goals text updated

--- themes/dockerz/components/about/_SveltinGoals.svelte
+++ themes/dockerz/components/about/_SveltinGoals.svelte
@@ -16,12 +16,12 @@
 		</p>
 
 		<p class="text-base font-light text-skin-body dark:text-skin-body-dark sm:text-lg">
-			Sveltin is a two side story:
+			Sveltin consists of three main things:
 		</p>
 		<div>
 			<ul class="list-none space-y-3 text-base sm:text-lg md:mt-5">
 				<li class="font-light text-skin-body dark:text-skin-body-dark">
-					↳ The CLI: built in Go, uses Cobra, Viper, PromptUI and others
+					↳ The CLI: built in Go, uses Cobra, Viper, Bubbletea, Lipgloss and others
 				</li>
 				<li class="font-light text-skin-body dark:text-skin-body-dark">
 					↳ The Template App: based on the stardard SvelteKit starter (<span
